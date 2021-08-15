db6a1a40f15aa46c19b5325f2abbc4829f2bf00b
Jan Richard Buniel <jrbuniel15@gmail.com>
2021-08-15 18:57:49 +0800

    Added login and register UI

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isEmailFocused: boolean;
+  isPasswordFocused: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  isFirstNameFocused: boolean;
+  isLastNameFocused: boolean;
+  isEmailFocused: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
